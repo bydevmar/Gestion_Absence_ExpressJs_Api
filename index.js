b6fd3724d1bd2f213e735fbe29328f9c7f4cc895
DevLop99 <83462650+DevLop99@users.noreply.github.com>
2021-05-10 20:39:20 +0000

    SET index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Welcome to GestionAbsence API!");
+const express = require("express");
+const mongoose = require('mongoose');
+
+
+//set up express app
+const app = express();
+
+const port = 3001 || process.env.port
+app.listen(port ,()=>{
+    console.log(`app connected to ${port}`);
+});
+
+//middleware 
+app.use(express.json());
+
+//connect to mongoDB
+const url = "mongodb://127.0.0.1/GestionAbsence"
+mongoose.connect(url,{ useNewUrlParser: true , useUnifiedTopology: true}).then(()=>{
+    console.log("mongodb is connected!!");
+});
