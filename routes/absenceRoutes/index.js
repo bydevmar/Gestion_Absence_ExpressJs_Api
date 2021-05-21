8fb38d2811d18b435b77e88742ddd6c98b3cd5d1
DevLop99 <83462650+DevLop99@users.noreply.github.com>
2021-05-21 22:33:00 +0000

    CREATE putAbsenceByAdmin.js

--- routes/absenceRoutes/index.js
+++ routes/absenceRoutes/index.js
@@ -1,8 +1,9 @@
 const getabsence  = require("./getAbsence.route");
-const postabsence  = require("./postAbsence.route");
-const putabsence  = require("./putAbsenceByFormateur.route");
-const deleteabsence  = require("./deleteAbsence.route");
+const postabsence = require("./postAbsence.route");
+const putabsencebyFormateur = require("./putAbsenceByFormateur.route");
+const putabsencebyAdmin = require("./putAbsenceByAdmin.route");
+const deleteabsence = require("./deleteAbsence.route");
 
-const routes = [getabsence,postabsence,putabsence,deleteabsence];
+const routes = [ getabsence , postabsence , putabsencebyFormateur , deleteabsence , putabsencebyAdmin ];
 
 module.exports = routes;
---
+++ routes/absenceRoutes/putAbsenceByAdmin.route.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+
+const Absence = require("../../models/Absence.model");
+const Utilisateur = require("../../models/Utilisateur.model");
+const Stagiaire = require("../../models/Stagiaire.model");
+const Affectation = require("../../models/Affectation.model");
+const absenceSchema = require('../../helpers/Absence.validator')
+
+
+router.put('/api/absences/:id_g/:id_a', (req, res) => {
+    absenceSchema.validateAsync(req.body)
+        .then(() => {
+            Utilisateur.findById(req.params.id_g)
+                .then(async (user) => {
+                    if (user.type == "Gestionnaire") {
+                        Stagiaire.findById(req.body.stagiaire).populate("groupe")
+                            .then((stagiaire) => {
+                                Affectation.findOne({ "groupe": stagiaire.groupe._id, "formateur": req.body.formateur })
+                                    .then((affectation) => {
+                                        if (affectation != null) {
+                                            Absence.updateOne({ _id: req.params.id_a }, req.body)
+                                                .then(() => {
+                                                    res.status(200)
+                                                        .send({
+                                                            status: "OK",
+                                                            message: "absences modifié avec succès!",
+                                                            details: req.body
+                                                        });
+                                                })
+                                        } else {
+                                            res.send({
+                                                status: "ERROR",
+                                                message: "Vous pouvez pas modifier l'absence à ce groupe!"
+                                            });
+                                        }
+                                    })
+                                    .catch(() => {
+                                        res.send({
+                                            status: "ERROR",
+                                            message: "Erreur lors de modification de cette absence!"
+                                        });
+                                    })
+                            }).catch(() => {
+                                res.send({
+                                    status: "ERROR",
+                                    message: "Aucun stagiaire avec ce ID!"
+                                });
+                            })
+                    }
+                    else{
+                        res.send({
+                            status: "ERROR",
+                            message: "Vos etes pas un administrateur!"
+                        });
+                    }
+                }).catch(() => {
+                    res.send({
+                        status: "ERROR",
+                        message: "Erreur lors de vérifications de votre identité!"
+                    });
+                })
+
+        }).catch((error) => {
+            res.send({
+                status: "ERROR",
+                message: error.details[0].message
+            });
+        })
+})
+
+module.exports = router;
